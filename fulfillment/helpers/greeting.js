3d71791a530a2c23db80f7cc49e2cd726b0ed7e2
ArjunThakurDev <arjunthakur@live.com>
2017-11-10 10:09:11 +0000

    added admin

--- fulfillment/helpers/greeting.js
+++ fulfillment/helpers/greeting.js
@@ -1,9 +1,11 @@
 var builder = require('botbuilder');
+var request = require("request");
 
 module.exports = [
 
     // Destination
-    function (session,args, next) {
+    function (session, args, next) {
+        var userrole;
         var userdetails = {
             "Get Flight details": {
                 value: "show me my flight details",
@@ -12,13 +14,50 @@ module.exports = [
                 value: "show me my hotel details",
             }
         };
-/*         if (session.userData.first_name) {
 
-            builder.Prompts.text(session, `Hello ${session.userData.first_name}. :)`);
-        } */
-        session.send('Welcome to TCS Aider help App');
-        builder.Prompts.choice(session, "Please choose 1 of the given options", userdetails);
-        next();
+        var offer_option = {
+            url: ' http://ghbotapi.azurewebsites.net/sasusers/',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'GET',
+            json: true
+        };
+        request(offer_option, function (error, response, body) {
+            if (error) {
+                console.log('Unable to get data ');
+                session.send(`We will get back to you with ancillary offers...`);
+                throw new Error(error);
+
+            } else {
+                for (var i = 0, len = body.length; i < len; i++) {
+                    if (body[i].FirstName + body[i].LastName === session.userData.first_name + session.userData.last_name) {
+                        userrole = body[i].Role;
+                    }
+                    if (userrole === "Admin") {
+                        userdetails.push([{
+                            "Get All User details": {
+                                value: "show all users details",
+                            },
+                        }]);
+                    }
+                }
+
+                session.send('Welcome to TCS Aider help App');
+                builder.Prompts.choice(session, "Please choose 1 of the given options", userdetails);
+                next();
+            }
+
+        });
+
+
+
+
+        /*         if (session.userData.first_name) {
+        
+                    builder.Prompts.text(session, `Hello ${session.userData.first_name}. :)`);
+                } */
+
     },
     // Check-in
     function (session, results) {
