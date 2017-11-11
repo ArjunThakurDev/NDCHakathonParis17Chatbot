2bc12e1c9560df784fe690f692108b92f4393f63
ArjunThakurDev <arjunthakur@live.com>
2017-11-11 14:17:03 +0000

    flight and hotel

--- fulfillment/helpers/Admin-helper.js
+++ fulfillment/helpers/Admin-helper.js
@@ -98,8 +98,8 @@ function create_cards(body, session_to_use) {
             .images([
                 builder.CardImage.create(session_to_use, body[i].ProfilePic),                
             ])
-            .buttons([builder.CardAction.imBack(session_to_use, 'hotel for ' + body[i].UserID, 'hotel for ' + body[i].FirstName),
-            builder.CardAction.imBack(session_to_use, ' flight for ' + body[i].UserID, 'flight for ' + body[i].FirstName)])
+            .buttons([builder.CardAction.imBack(session_to_use, 'you have selected hotel details for ' + body[i].UserID, 'hotel for ' + body[i].FirstName),
+            builder.CardAction.imBack(session_to_use, 'you have selected flight details for ' + body[i].UserID, 'flight for ' + body[i].FirstName)])
         cards.push(card);
     }
     console.log(JSON.stringify(cards));
