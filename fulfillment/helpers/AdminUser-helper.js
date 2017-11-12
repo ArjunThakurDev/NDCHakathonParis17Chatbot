4eb99a262a1d3966f9190d12ff9f7f2ca35e84ef
ArjunThakurDev <arjunthakur@live.com>
2017-11-12 09:13:05 +0000

    added query button for flight

--- fulfillment/helpers/AdminUser-helper.js
+++ fulfillment/helpers/AdminUser-helper.js
@@ -77,7 +77,7 @@ function create_cards(body, session_to_use, object) {
                 .images([
                     builder.CardImage.create(session_to_use, body[i].flightpic)
                 ])
-                .buttons([builder.CardAction.imBack(session_to_use, 'Flight Details for ' + body[i].FlightNo)]);
+                .buttons([builder.CardAction.imBack(session_to_use,"tel:" + "+919910665326" ,'Call for Qauery')]);
             cards.push(card);
 
         }
